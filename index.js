6e430ff88bb5d50faa7c2a4719001071ae38db6d
Aaron Young <aarony422@gmail.com>
2020-07-05 18:56:04 -0700

    added node version in package.json, using the right port number, adding Procfile for heroku

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+var port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+http.listen(port, () => {
+  console.log('listening on *:', port);
 });
